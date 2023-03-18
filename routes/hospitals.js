8a5bd66a01195aa9ba8657ba7d1ebf4a5028bf2f
Pattarapong <rutkiller@gmail.com>
2023-03-18 13:47:56 +0700

    Assignment_7_Relationships

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -9,7 +9,7 @@ const {protect, authorize} = require('../middleware/auth');
 // Re-route into other resource routers 
 router.use('/:hospitalId/appointments/', appointmentRouter);
 
-router.route('/').get(getHospitals).post(protect,authorize('admin'), createHospital);
-router.route('/:id').get(getHospital).put(protect,authorize('admin'), updateHospital).delete(protect, authorize('admin'), deleteHospital);
+router.route('/').get(protect,getHospitals).post(protect,authorize('user','admin'), createHospital);
+router.route('/:id').get(protect,getHospital).put(protect,authorize('user','admin'), updateHospital).delete(protect, authorize('admin'), deleteHospital);
 
 module.exports=router;
